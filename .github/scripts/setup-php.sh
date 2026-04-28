5ac99df1c44bbed4ac4087c6b701345152088448
Bruno Gaspar <brunofgaspar1@gmail.com>
2026-04-28 14:05:17 +0100

    Chore/sync config files (#10)
    * chore: standardize composer auth format
    
    * chore: update CI action versions and runner
    
    * chore: add release-drafter workflow
    
    * chore: standardize setup-php.sh
    
    * chore: fix CI workflow triggers
    
    * chore: fix composer auth placement in CI workflows
    
    * chore: remove unnecessary COMPOSER_AUTH from CI workflows
    
    * Linting
    
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>
    
    ---------
    
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- .github/scripts/setup-php.sh
+++ .github/scripts/setup-php.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
+sudo apt-get install python3-launchpadlib
 sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update
 sudo apt-get install -y \
     php${PHP_VERSION} \
     php${PHP_VERSION}-cli \
-    php${PHP_VERSION}-dev \
-    php${PHP_VERSION}-fpm \
     php${PHP_VERSION}-mbstring \
     php${PHP_VERSION}-xml
